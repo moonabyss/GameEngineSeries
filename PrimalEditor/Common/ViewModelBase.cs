210a1995fcc22bfcdebeac229070d1865933b215
moonabyss <d-v@ukr.net>
2024-10-18 22:34:20 +0200

    002 - Loading project templates for game projects

---
+++ PrimalEditor/Common/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+
+namespace PrimalEditor
+{
+public class ViewModelBase : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected void OnPropertyChanged(string propertyName) { PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName)); }
+}
+}

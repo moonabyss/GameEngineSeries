b2fe44bcc94cb0ff521f6e465f3fd674f379b9cb
moonabyss <d-v@ukr.net>
2025-01-21 01:50:01 +0100

    003 - Implement creating new game projects

--- PrimalEditor/GameProject/NewProjectView.xaml.cs
+++ PrimalEditor/GameProject/NewProjectView.xaml.cs
@@ -21,5 +21,19 @@ namespace PrimalEditor.GameProject
     public partial class NewProjectView : UserControl
     {
         public NewProjectView() { InitializeComponent(); }
+
+        private void OnCreate_Button_Click(object sender, RoutedEventArgs e)
+        {
+            var vm = DataContext as NewProject;
+            var projectPath = vm.CreateProject(templateListBox.SelectedItem as ProjectTemplate);
+            bool dialogResult = false;
+            var win = Window.GetWindow(this);
+            if (!string.IsNullOrEmpty(projectPath))
+            {
+                dialogResult = true;
+            }
+            win.DialogResult = dialogResult;
+            win.Close();
+        }
     }
 }
